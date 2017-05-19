063b02e4477de83b5e3ef1e5769b0fc78bb0e6f1
Jspsun <Jspsun@gmail.com>
2017-05-19 18:53:59 -0400

    fixed fullscreen issue

--- Source/sketch.js
+++ Source/sketch.js
@@ -4,86 +4,81 @@
 // One vehicle "seeks"
 // See: http://www.red3d.com/cwr/
 
-var vehicles=[];
+var vehicles = [];
 
-var food=[];
+var food = [];
 
-var poison=[]
+var poison = [];
 
 var debug;
 
-function setup() {
-  createCanvas(3*windowWidth/4,windowHeight);
-  for (var i=0;i<50;i++){
-    var x= random(width);
+function setup () {
+  createCanvas(windowWidth, windowHeight);
+  for (var i = 0;i < 50;i++) {
+    var x = random(width);
     var y = random(height);
     vehicles[i] = new Vehicle(x, y);
-
   }
-  for (var i =0; i<40;i++){
-    var x= random(width);
-    var y= random(height);
-    food.push(createVector(x,y));
+  for (var i = 0; i < 40;i++) {
+    var x = random(width);
+    var y = random(height);
+    food.push(createVector(x, y));
   }
-  for (var i =0; i<20;i++){
-    var x= random(width);
-    var y= random(height);
-    poison.push(createVector(x,y));
+  for (var i = 0; i < 20;i++) {
+    var x = random(width);
+    var y = random(height);
+    poison.push(createVector(x, y));
   }
-  debug= createCheckbox();
+  debug = createCheckbox();
 }
 
-
-
-function draw() {
+function draw () {
   background(51);
 
-  if (random(1)<0.08){
-    var x= random(width);
-    var y= random(height);
-    food.push(createVector(x,y));
+  if (random(1) < 0.08) {
+    var x = random(width);
+    var y = random(height);
+    food.push(createVector(x, y));
   }
 
-  if (random(1)<0.01){
-    var x= random(width);
-    var y= random(height);
-    poison.push(createVector(x,y));
+  if (random(1) < 0.01) {
+    var x = random(width);
+    var y = random(height);
+    poison.push(createVector(x, y));
   }
 
-  for (var i=0;i<food.length;i++){
-    fill(0,255,0);
-      noStroke();
-    ellipse(food[i].x, food[i].y,3,3);
+  for (var i = 0;i < food.length;i++) {
+    fill(0, 255, 0);
+    noStroke();
+    ellipse(food[i].x, food[i].y, 3, 3);
   }
-  for (var i=0;i<poison.length;i++){
-    fill(255,0,0);
-      noStroke();
-    ellipse(poison[i].x, poison[i].y,3,3);
+  for (var i = 0;i < poison.length;i++) {
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(poison[i].x, poison[i].y, 3, 3);
   }
-  for (var i=vehicles.length-1;i>=0;i--){
+  for (var i = vehicles.length - 1;i >= 0;i--) {
     // Call the appropriate steering behaviors for our agents
     vehicles[i].boundaries();
-    vehicles[i].behaviors(food,poison);
+    vehicles[i].behaviors(food, poison);
     vehicles[i].update();
     vehicles[i].display();
 
     var newVehicle = vehicles[i].cloneMe();
-    if (newVehicle!=null){
+    if (newVehicle != null) {
       vehicles.push(newVehicle);
     }
 
-    if (vehicles[i].dead()){
-      var x=vehicles[i].position.x;
-      var x=vehicles[i].position.y;
+    if (vehicles[i].dead()) {
+      var x = vehicles[i].position.x;
+      var x = vehicles[i].position.y;
 
-      food.push(createVector(x,y));
-      vehicles.splice(i,1);
-
+      food.push(createVector(x, y));
+      vehicles.splice(i, 1);
     }
-
   }
 }
 
-function windowResized() {
-  resizeCanvas(3*windowWidth/4, windowHeight);
+function windowResized () {
+  resizeCanvas(3 * windowWidth / 4, windowHeight);
 }
